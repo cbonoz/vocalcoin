a2bc7ed216fc012b040b99f2874598579d71dd8e
cbonoz <chrisdistrict@gmail.com>
2018-02-03 15:19:40 -0500

    add sample code for creating a new stellar wallet and fetching the balances

--- vocal/server/stellar.js
+++ vocal/server/stellar.js
@@ -1,6 +1,44 @@
 const library = (function () {
 
+  const StellarSdk = require('stellar-sdk');
+  const request = require('request');
+  const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+  // create a completely new and unique pair of keys
+  // see more about KeyPair objects: https://stellar.github.io/js-stellar-sdk/Keypair.html
+
+  const createKeyPair = () => {
+    const pair = StellarSdk.Keypair.random();
+    // pair.secret();
+    // pair.publicKey();
+    return pair;
+  }
+
+  const createAccount = (pair, cb) => {
+    request.get({
+        url: 'https://horizon-testnet.stellar.org/friendbot',
+        qs: { addr: pair.publicKey() },
+        json: true
+    }, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            console.error('ERROR!', error || body);
+        }
+        else {
+
+            cb(body);
+        }
+    });
+  }
+
+  const getBalances = (pair, cb) => {
+    // the JS SDK uses promises for most actions, such as retrieving an account
+    server.loadAccount(pair.publicKey()).then(cb);
+  }
+
   return {
+    createKeyPair: createKeyPair,
+    createAccount: createAccount,
+    getBalances: getBalances
   };
 
 })();
--- vocal/server/test.js
+++ vocal/server/test.js
@@ -1,14 +1,29 @@
+
 const escape = require('pg-escape');
-
-var sql = escape("INSERT INTO issues(user_id, description, title, lat, lng, place, active, time) values('EQo9MtWq9wWd3LmPJaJUX8F25rG2', 'test', 'test title', 41.87515838725938, -87.6318856454468, %L, true, 1514591624548", "Boston Blackie's");
-console.log(sql);
+const StellarSdk = require('stellar-sdk');
 
 
-// const w = wallet.keystore.createVault()
-// console.log(w)
-// Using the Ethers umbrella package...
-var ethers = require('ethers');
-var Wallet = ethers.Wallet;
+// Custom libraries
+const stellar = require('./stellar');
 
-const w = Wallet.createRandom();
-console.log(w);
+// var sql = escape("INSERT INTO issues(user_id, description, title, lat, lng, place, active, time) values('EQo9MtWq9wWd3LmPJaJUX8F25rG2', 'test', 'test title', 41.87515838725938, -87.6318856454468, %L, true, 1514591624548", "Boston Blackie's");
+// console.log(sql);
+
+const keyPair = stellar.createKeyPair();
+console.log('keyPair', keyPair.secret(), keyPair.publicKey())
+var account = null;
+
+stellar.createAccount(keyPair, (body) => {
+    account = body;
+    console.log('Account: ' + JSON.stringify(account));
+
+    // Get balances for the newly created account.
+    stellar.getBalances(keyPair, (account) => {
+        console.log('Balances for account: ' + keyPair.publicKey());
+        account.balances.forEach(function (balance) {
+            console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+        })
+    });;
+
+});
+
